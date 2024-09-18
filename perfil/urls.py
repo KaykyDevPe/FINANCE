9beafe40d79a4f0c480f4280bb2e0df2b5b0182f
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-09-18 08:08:41 -0300

    01 - FINANCE - 40:50

--- perfil/urls.py
+++ perfil/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name="home"),
-    path('gerenciar/', views.gerenciar, name="gerenciar")
+    path('gerenciar/', views.gerenciar, name="gerenciar"),
+    path('cadastrar_banco', views.cadastrar_banco, name="cadastrar_banco"),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Conta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def home(request):
     return render(request, 'home.html')
 
 def gerenciar(request):
-    return render(request, 'gerenciar.html')
+    contas = Conta.objects.all()
+    total_contas = 0
+    
+    for conta in contas:
+        total_contas += conta.valor
+    
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas})
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        return redirect('/perfil/gerenciar')
+    
+    conta = Conta(
+        apelido=apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+    
+    conta.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
+    return redirect('/perfil/gerenciar')
+    
