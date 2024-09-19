63cec3d366934f5f42df50d90a944e1226aeba78
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-09-19 07:53:03 -0300

    01 - FINANCE - FINALIZADA

--- perfil/views.py
+++ perfil/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Conta
+from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
+from .utils import calcula_total
 
 def home(request):
-    return render(request, 'home.html')
+    contas = Conta.objects.all()
+        
+    total_contas = calcula_total(contas, 'valor')
+    
+    return render(request, 'home.html', {'contas': contas, 'total_contas': total_contas,})
 
 def gerenciar(request):
     contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
     total_contas = 0
     
-    for conta in contas:
-        total_contas += conta.valor
+    total_contas = calcula_total(contas, 'valor')
     
-    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas})
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -38,4 +43,33 @@ def cadastrar_banco(request):
     conta.save()
     messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
     return redirect('/perfil/gerenciar')
+
+def deletar_banco(request, id):
+    conta = Conta.objects.get(id=id)
+    conta.delete()
     
+    messages.add_message(request, constants.SUCCESS, 'Conta deletada com sucesso')
+    return redirect('/perfil/gerenciar')
+
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+    
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+    
+    categoria.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso.')
+    return redirect('/perfil/gerenciar')
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+    categoria.essencial = not categoria.essencial
+    categoria.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Categoria atualizada com sucesso')
+    return redirect('/perfil/gerenciar')
