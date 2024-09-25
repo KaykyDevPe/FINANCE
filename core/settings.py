1151b9d9b2a1bfc375b0c056e660fbb239aaa2af
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-09-25 08:11:37 -0300

    02 - FINANCE - 26:36

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'perfil',
     'extrato',
+    'planejamento',
 ]
 
 MIDDLEWARE = [
--- perfil/models.py
+++ perfil/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+
 
 class Categoria(models.Model):
     categoria = models.CharField(max_length=50)
@@ -8,6 +10,18 @@ class Categoria(models.Model):
     def __str__(self):
         return self.categoria
     
+    def total_gasto(self):
+        from extrato.models import Valores
+        valores = Valores.objects.filter(categoria__id = self.id).filter(data__month = datetime.now().month).filter(tipo='S')
+        
+        total_valor = 0
+        for valor in valores:
+            total_valor += valor.valor
+        return total_valor
+    
+    def calcula_percentual_gasto_por_categoria(self):
+        return int((self.total_gasto() * 100) / self.valor_planejamento)
+        
 class Conta(models.Model):
     banco_choices = (
         ('NU', 'Nubank'),
